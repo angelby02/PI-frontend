4d2adffcd53f20cca0a69483a3a883e6bf889265
angelby02 <laf.vargas@yavirac.edu.ec>
2023-12-11 19:39:11 -0500

    terminos y condiciones-navbar

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -31,11 +31,13 @@ get emailNoValido(){
 get passwordNoValido(){
   return this.registerForm.get('password')?.invalid && this.registerForm.get('password')?.touched;
 }
+
   ngOnInit(): void{
     this.registerForm=this.formBuilder.group({
       username:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
       email: ['',[Validators.required,Validators.email,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       password: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(11)]],
+
     });
   }
 
